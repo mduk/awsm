4e5bcf7c2f570336f00bdd8f33342c435e2569e7
Daniel Kendell <daniel.kendell@mendeley.com>
2014-12-05 13:47:36 +0000

    fixed seriously fucked indentation

--- lib/awsome/instances.rb
+++ lib/awsome/instances.rb
@@ -1,23 +1,21 @@
 module Awsome
-	class Instances
+  class Instances
+    def initialize
+      @client = Aws::EC2::Client.new
+    end
 
-		def initialize
-			@client = Aws::EC2::Client.new
-		end
+    def get_instance_data( instance_ids )
+      descriptions = @client.describe_instances( {
+        filters: [
+          { name: "instance-id", values: instance_ids }
+        ]
+      } ).reservations.first.instances
 
-		def get_instance_data( instance_ids )
-			descriptions = @client.describe_instances( {
-				filters: [
-					{ name: "instance-id", values: instance_ids }
-				]
-			} ).reservations.first.instances
-
-			instance_hash = {}
-			descriptions.each do |description|
-				instance_hash[ description.instance_id ] = description.to_h
-			end
-			instance_hash
-		end
-
-	end
+      instance_hash = {}
+      descriptions.each do |description|
+        instance_hash[ description.instance_id ] = description.to_h
+      end
+      instance_hash
+    end
+  end
 end
