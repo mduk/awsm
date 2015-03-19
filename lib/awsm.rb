c9fb80c77432a508e1154b28fd00b830a99399d6
Daniel Kendell <daniel@kendell.org>
2015-03-19 11:13:46 +0000

    double to single quotes

--- lib/awsm.rb
+++ lib/awsm.rb
@@ -1,11 +1,14 @@
-require "awsm/version"
-require "awsm/loadbalancers"
-require "awsm/autoscalinggroups"
-require "awsm/dns"
-require "awsm/instances"
+require 'yaml'
 
-require "awsm/cli/spin"
-require "awsm/cli"
+require 'awsm/config'
+require 'awsm/version'
+require 'awsm/loadbalancers'
+require 'awsm/autoscalinggroups'
+require 'awsm/dns'
+require 'awsm/instances'
+
+require 'awsm/cli/spin'
+require 'awsm/cli'
 
 module Awsm
 end
