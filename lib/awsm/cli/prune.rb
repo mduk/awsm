a9a6979feed6f729846a82f37ed5fbbd6cabb04b
Daniel Kendell <daniel@kendell.org>
2015-03-20 21:41:31 +0000

    list unused launch configurations

--- lib/awsm/cli/prune.rb
+++ lib/awsm/cli/prune.rb
@@ -16,9 +16,24 @@ module Awsm
         )
       end
 
+      desc 'launchconfigurations',
+        "Find unused launch configurations"
+      def launchconfigurations
+        puts Terminal::Table.new(
+          headings: [ "Launch Configuration Name", "Created Time" ],
+          rows: find_unused_launchconfigurations.map do |lc|
+            [ lc.launch_configuration_name, lc.created_time ]
+          end
+        )
+      end
+
       no_commands do
         def elb
           Aws::ElasticLoadBalancing::Client.new
+        end
+
+        def as
+          Aws::AutoScaling::Client.new
         end
 
         def find_instanceless_elbs
@@ -29,6 +44,21 @@ module Awsm
             end
           end
           elbs
+        end
+
+        def find_unused_launchconfigurations
+          used_launchconfiguration_names = []
+          as.describe_auto_scaling_groups.auto_scaling_groups.each do |asg|
+            used_launchconfiguration_names << asg.launch_configuration_name
+          end
+
+          launchconfigurations = []
+          as.describe_launch_configurations.launch_configurations.each do |lc|
+            unless used_launchconfiguration_names.include?( lc.launch_configuration_name )
+              launchconfigurations << lc
+            end
+          end
+          launchconfigurations
         end
       end
     
